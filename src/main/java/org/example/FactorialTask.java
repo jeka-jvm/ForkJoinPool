c89129c15b33a835d13837531b028d29226c068f
jeka <z_jeka_jvm@inbox.ru>
2024-07-31 05:15:15 +0300

    initial commit

---
+++ src/main/java/org/example/FactorialTask.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.concurrent.RecursiveTask;
+
+
+public class FactorialTask extends RecursiveTask<Long> {
+
+    private final int n;
+
+    public FactorialTask(int n) {
+        this.n = n;
+    }
+
+    @Override
+    protected Long compute() {
+        if (n <= 1) {
+            return 1L;
+        }
+
+        FactorialTask subTask = new FactorialTask(n - 1);
+        subTask.fork();
+
+        return n * subTask.join();
+    }
+}
